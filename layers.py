ca907ab84a87ac5f3f3835756fd768fceeffcade
suntiecheng <suntiecheng@baidu.com>
2017-09-07 13:43:35 +0800

    update

--- layers.py
+++ layers.py
@@ -1,14 +1,31 @@
 def affine_forward(X, W, b):
 	out = None
-	# x.shape == (N, D)
-	# W.shape == (D, M)
-	# b.shape == (1, M)
+	# x.shape == (N, D1, D2, ...)
+	# W.shape == (D1 * D2 * ..., M)
+	# b.shape == (M, )
+
 	N, D = X.shape
 	_, M = W.shape
 	
 	y = X.reshape(N, -1).dot(W) + b
+
 	cache = (X, W, b)
 
 	return out, cache
+
+
+def affine_backward(dout, cache):
+
+	N, M = dout.shape
+
+	X, W, b = cache
+
+	dX = dout.dot(W.T).reshape(*X.shape)
+
+	dW = np.dot(X.T, dout)
+
+	db = np.sum(dout, axis=0)
+
+	return dX, dW, db
 
 
---
+++ relativeError.py
@@ -0,0 +1,3 @@
+def relative_error(A, B):
+    abs = np.abs(A - B) / (np.maximum(1e-8, np.abs(A) + np.abs(B)))
+    return np.max(abs)
