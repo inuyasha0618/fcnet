f29730ee9210b214e1f7d31706644f80bdafb6e9
suntiecheng <suntiecheng@baidu.com>
2017-09-09 11:25:05 +0800

    relu forward backward complete

--- layers.py
+++ layers.py
@@ -1,12 +1,8 @@
 import numpy as np
 
 def affine_forward(X, W, b):
-	out = None
-	# x.shape == (N, D1, D2, ...)
-	# W.shape == (D1 * D2 * ..., M)
-	# b.shape == (M, )
 
-	# print(X.shape)
+	out = None
 
 	N = X.shape[0]
 
@@ -34,5 +30,49 @@ def affine_backward(dout, cache):
 	db = np.sum(dout, axis=0)
 
 	return dX, dW, db
+
+def relu_forward(X):
+
+	cache = X > 0
+
+	out = np.maximum(0, X)
+
+	return out, cache
+
+def relu_backward(dout, cache):
+
+	pos_mask = cache
+
+	local_derivative = np.zeros(pos_mask.shape)
+
+	local_derivative[pos_mask] = 1
+
+	dX = local_derivative * dout
+
+	return dX
+
+def affine_relu_forward(X, W, b):
+
+	affine_out, affine_cache = affine_forward(X, W, b)
+
+	out, relu_cache = relu_forward(affine_out)
+
+	cache = (affine_cache, relu_cache)
+
+	return out, cache
+
+def affine_relu_backward(dout, cache):
+
+	affine_cache, relu_cache = cache
+
+	daffine_out = relu_backward(dout, relu_cache)
+
+	dX, dW, db = affine_backward(daffine_out, affine_cache)
+
+	return dX, dW, db
+
+def softmax_loss(X, y):
+
+	
 
 
