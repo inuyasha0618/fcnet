401f5e337423297975b346cde39f55348ddc9a1d
inuyasha0618 <chenjiandiwang@qq.com>
2017-09-10 17:00:32 +0800

    Trainer 解构搭建

---
+++ Trainer.py
@@ -0,0 +1,13 @@
+class Trainer(object):
+	"""docstring for Trainer"""
+	def __init__(self, model=None, learning_rate=1e-3, batch_size=10, iter_nums=1000):
+		
+		self.model = model		
+		
+	def _step(self):
+		pass
+
+	def train(self):
+		pass
+
+
