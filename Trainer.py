d8adfeec7c4a377a5b0f4807b591c59cf1f9ce45
inuyasha0618 <chenjiandiwang@qq.com>
2017-09-11 07:48:50 +0800

    Trainer基本实现正确

--- Trainer.py
+++ Trainer.py
@@ -1,25 +1,50 @@
+import numpy as np
+
 class Trainer(object):
 	"""docstring for Trainer"""
-	def __init__(self, model=None, X=None, y=None, learning_rate=1e-3, batch_size=10, epoch_nums=1000):
+	def __init__(self, model, data, learning_rate=1e-3, batch_size=10, epoch_nums=100):
 		
 		self.model = model
-		self.X = X
-		self.y = y
+		self.X_train = data['X_train']
+		self.y_train = data['y_train']
+		self.X_val = data['X_val']
+		self.y_val = data['y_val']
 		self.learning_rate = learning_rate
 		self.batch_size = batch_size
-		self.iter_nums = iter_nums	
+		self.epoch_nums = epoch_nums	
 		
-	def _step(self, batch_x, batch_y):
+	def _step(self):
 
-		loss, grads = self.model.loss(batch_x, batch_y)
+		N = self.X_train.shape[0]
+
+		mask = np.random.choice(N, self.batch_size)
+
+		X_batch = self.X_train[mask]
+
+		y_batch = self.y_train[mask]
+
+		loss, grads = self.model.loss(X_batch, y_batch)
 		
 		for param_key, grad in grads.items():
-			self.model.params[param_key] -= learning_rate * grad
+
+			self.model.params[param_key] -= self.learning_rate * grad
+
+		return loss
 
 	def train(self):
 
-		N = self.X.shape[0]
+		N = self.X_train.shape[0]
 		
-		iters_per_epoch = N // self.batch_size
+		iters_per_epoch = max(1, N // self.batch_size)
 
-		
+		iter_nums = self.epoch_nums * iters_per_epoch
+
+		loss_cache = []
+
+		for i in range(iter_nums):
+
+			loss = self._step()
+
+			loss_cache.append(loss)
+
+		return loss_cache
